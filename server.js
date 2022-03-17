cdf3bab2c953da80a6500e07b8829007b97632cd
Fasasi Toheeb <tohebayomide11@gmail.com>
2022-03-17 12:17:54 +0100

    chore: finish the implementation detail

--- server.js
+++ server.js
@@ -16,15 +16,29 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', (socket) => {
+  let selectedClient = []
   console.log(`client connected with id ${socket.id}`);
-  console.log(socket)
 
   socket.on('chat-message', (data) => {
     const {type, message} = data;
 
+    // This holds all the user which notification want to be sent to
     const res = check(type);
 
-    io.emit('hello',data)
+    // REAL TIME NOTIFICATION
+    /**
+    * I think socket.io should be able to expose the express reqeust object as well as all what we have define on the request object.
+    * e.g like the user object, which holds certain data about the logged in user
+    */
+    const user = socket.request.user
+    if(user === type) selectedClient = [...selectedClient, socket.id]
+
+    /**
+    * Emitting real time notification to user the match the type provided from the frontend
+    */
+    for (let id in selectedClient) {
+      io.to(selectedClient[id]).emit(message)
+    }
   });
 
   io.emit('test', {test: 'testing'})
